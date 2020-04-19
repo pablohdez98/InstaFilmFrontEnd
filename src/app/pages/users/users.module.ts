df5eb7571b74efadb1560287468c3b54f689753d
Javier Sintes <javier@secret-source.eu>
2020-04-19 16:37:25 +0100

    Design project structure

---
+++ src/app/pages/users/users.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UsersRoutingModule } from './users-routing.module';
+import { UsersComponent } from './users.component';
+import { UsersListComponent } from './users-list/users-list.component';
+import { DeleteUserComponent } from './delete-user/delete-user.component';
+import { UpdateUserComponent } from './update-user/update-user.component';
+import { CreateUserComponent } from './create-user/create-user.component';
+
+
+@NgModule({
+  declarations: [UsersComponent, UsersListComponent, DeleteUserComponent, UpdateUserComponent, CreateUserComponent],
+  imports: [
+    CommonModule,
+    UsersRoutingModule
+  ]
+})
+export class UsersModule { }
