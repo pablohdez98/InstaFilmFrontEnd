f6b7f25ee0f7d03a7231885667fc4b7bc443cdf8
Javier Sintes <javier@secret-source.eu>
2020-04-19 17:28:20 +0100

    Add users components routes

--- src/app/pages/users/users-routing.module.ts
+++ src/app/pages/users/users-routing.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { UsersComponent } from './users.component';
+import {UsersListComponent} from "./users-list/users-list.component";
+import {CreateUserComponent} from "./create-user/create-user.component";
+import {UpdateUserComponent} from "./update-user/update-user.component";
+import {DeleteUserComponent} from "./delete-user/delete-user.component";
 
 const routes: Routes = [
   {
-    path: '', component: UsersComponent
+    path: '', component: UsersComponent,
+    children:[
+      {
+        path: 'users-list', component: UsersListComponent,
+      },
+      {
+        path: 'create', component: CreateUserComponent,
+      },
+      {
+        path: 'update/:id', component: UpdateUserComponent,
+      },
+      {
+        path: 'delete', component: DeleteUserComponent,
+      },
+    ]
   }
   ];
 
