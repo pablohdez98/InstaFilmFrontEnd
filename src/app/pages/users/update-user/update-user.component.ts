89fbbd5ae046fe022207787ed7f21fd17b804f50
Javier Sintes <javier@secret-source.eu>
2020-04-20 18:26:10 +0100

    Add update user view

--- src/app/pages/users/update-user/update-user.component.ts
+++ src/app/pages/users/update-user/update-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-update-user',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-user.component.scss']
 })
 export class UpdateUserComponent implements OnInit {
+  public updateForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.updateForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      role: ['', Validators.required],
+      category: ['', Validators.required],
+      information: '',
+      image:'',
+    })
+  }
+  onSubmit(form){
+    console.log(form);
   }
 
 }
