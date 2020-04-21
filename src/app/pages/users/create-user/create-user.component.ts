26633897615d217c11a163ef5c620610bb182a78
Javier Sintes <javier@secret-source.eu>
2020-04-21 18:36:36 +0100

    Validate create-user form

--- src/app/pages/users/create-user/create-user.component.ts
+++ src/app/pages/users/create-user/create-user.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {UserService} from "../../../services/user.service";
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-create-user',
@@ -8,13 +10,19 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 })
 export class CreateUserComponent implements OnInit {
   public createForm: FormGroup;
+  public seePassword: boolean;
+  faEye = faEye;
+  faEyeSlash = faEyeSlash;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder,
+              private userService: UserService) {
+    this.seePassword = false;
+  }
 
   ngOnInit() {
     this.createForm = this.formBuilder.group({
       name: ['', Validators.required],
-      surname: ['', Validators.required],
+      lastName: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
       role: ['', Validators.required],
@@ -23,8 +31,21 @@ export class CreateUserComponent implements OnInit {
       image:'',
     })
   }
+  get FormControl() {
+    return this.createForm.controls;
+  }
+  getSeePassword(): boolean {
+    return this.seePassword;
+  }
+  toggleSeePassword(): void {
+    this.seePassword = !this.seePassword;
+  }
   onSubmit(form){
-    console.log(form);
+    if(this.createForm.status === 'VALID'){
+      this.userService.createUser(form).subscribe(res => console.log(res));
+    } else {
+      this.createForm.markAllAsTouched();
+    }
   }
 
 }
