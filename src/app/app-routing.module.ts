29f029fd36ad6fa0a810f74612f1c12fa41ef7df
Pablo Hernández <pablohc224@gmail.com>
2020-05-05 18:20:12 +0100

    Add new routes for films and series

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,9 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule),
   },
-  { path: 'users', loadChildren: () => import('./pages/users/users.module').then(m => m.UsersModule) }
+  { path: 'users', loadChildren: () => import('./pages/users/users.module').then(m => m.UsersModule) },
+  { path: 'movies', loadChildren: () => import('./pages/movies/movies.module').then(m => m.MoviesModule) },
+  { path: 'series', loadChildren: () => import('./pages/series/series.module').then(m => m.SeriesModule) }
 ];
 
 @NgModule({
