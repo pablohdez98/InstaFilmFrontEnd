0807c0a123678eaf1b331354155e9a0b9d2b0202
Pablo Hernández <pablohc224@gmail.com>
2020-05-05 18:23:36 +0100

    Add imports in series module

--- src/app/pages/series/series.module.ts
+++ src/app/pages/series/series.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { SeriesRoutingModule } from './series-routing.module';
 import { SeriesComponent } from './series.component';
+import { CreateSeriesComponent } from './create-series/create-series.component';
+import { DeleteSeriesComponent } from './delete-series/delete-series.component';
+import { UpdateSeriesComponent } from './update-series/update-series.component';
+import { ListSeriesComponent } from './list-series/list-series.component';
+import {ReactiveFormsModule} from '@angular/forms';
+
+
 @NgModule({
   declarations: [SeriesComponent, CreateSeriesComponent, DeleteSeriesComponent, UpdateSeriesComponent, ListSeriesComponent],
   imports: [
     CommonModule,
     SeriesRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class SeriesModule { }
