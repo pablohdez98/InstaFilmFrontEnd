3c60e76913312ba71975766e38bb3e64514d715f
Pablo Hernández <pablohc224@gmail.com>
2020-05-06 18:32:02 +0100

    Add navbar with logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'InstaFilm';
+
+  constructor(private router: Router) {}
+
+  async logout() {
+    localStorage.clear();
+    await this.router.navigate(['login']);
+  }
 }
