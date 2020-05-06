424ca763e57b7d993714cfc2db60ac9b777f3e19
Javier Sintes <javier@secret-source.eu>
2020-05-06 18:21:43 +0100

    Fix film-admin module imports

--- src/app/pages/films-admin/films-admin.module.ts
+++ src/app/pages/films-admin/films-admin.module.ts
@@ -4,13 +4,11 @@ import { CommonModule } from '@angular/common';
 import { FilmsAdminRoutingModule } from './films-admin-routing.module';
 import { FilmsAdminComponent } from './films-admin.component';
 import {CreateFilmsAdminComponent} from "./create-films-admin/create-films-admin.component";
-import { UpdateFilmsComponent } from './update-films/update-films.component';
-import { ListFilmsComponent } from './list-films/list-films.component';
 import {ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
-  declarations: [FilmsAdminComponent, CreateFilmsAdminComponent, UpdateFilmsComponent, ListFilmsComponent],
+  declarations: [FilmsAdminComponent, CreateFilmsAdminComponent],
   imports: [
     CommonModule,
     FilmsAdminRoutingModule,
