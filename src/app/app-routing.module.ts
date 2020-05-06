e1b00d0085925bb6b484387b3c016bc04d5291f3
Pablo Hernández <pablohc224@gmail.com>
2020-05-06 18:25:54 +0100

    Add signup vied and functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,8 @@ import {Routes, RouterModule, PreloadAllModules} from '@angular/router';
 
 
 const routes: Routes = [
-  {
-    path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule),
-  },
+  { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
+  { path: 'signup', loadChildren: () => import('./pages/signup/signup.module').then(m => m.SignupModule) },
   { path: 'users', loadChildren: () => import('./pages/users/users.module').then(m => m.UsersModule) },
   { path: 'admin/films', loadChildren: () => import('./pages/films-admin/films-admin.module').then(m => m.FilmsAdminModule) },
   { path: 'admin/series', loadChildren: () => import('./pages/series/series.module').then(m => m.SeriesModule) }
---
+++ src/app/pages/signup/signup.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {UserService} from '../../services/user.service';
+import {Router} from '@angular/router';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss']
+})
+export class SignupComponent implements OnInit {
+  public signupForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder,
+              private userService: UserService,
+              private router: Router) { }
+
+  ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(4)]],
+      lastName: ['', [Validators.required, Validators.minLength(4)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+  get FormControl() {
+    return this.signupForm.controls;
+  }
+
+  async onSubmit(form) {
+    if (this.signupForm.status === 'VALID') {
+      this.userService.signup(form).subscribe(
+        async () => {
+          await this.router.navigate(['/login']);
+        },
+        async () => {
+          await Swal.fire({
+            title: 'Error',
+            text: 'Hubo un error a la hora de registrarte',
+            icon: 'error',
+            confirmButtonText: 'OK',
+          });
+        }
+      );
+    } else {
+      this.signupForm.markAllAsTouched();
+    }
+  }
+}
