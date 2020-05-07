bb7636192c71a23d209ca57390315ee2ddd1fca2
Pablo Hernández <pablohc224@gmail.com>
2020-05-07 19:03:04 +0100

    Add navbar functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {UserService} from './services/user/user.service';
+import {User} from './services/user/user';
+import {JwtHelperService} from '@auth0/angular-jwt';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'InstaFilm';
+  currentUser: User;
+  role: number;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router,
+              private userService: UserService,
+              private jwtHelperService: JwtHelperService) {}
 
-  async logout() {
-    localStorage.clear();
-    await this.router.navigate(['login']);
+  ngOnInit() {
+    this.userService.currentUser.subscribe(user => {
+      this.currentUser = user;
+      this.role = this.jwtHelperService.decodeToken(user.access_token).role;
+    });
+  }
+  logout() {
+    this.userService.logout();
   }
 }
