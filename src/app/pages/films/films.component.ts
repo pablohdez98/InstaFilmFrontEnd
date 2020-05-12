0451e37d4a7219901ce821df4925bd6aa266dc89
Javier Sintes <javier@secret-source.eu>
2020-05-12 13:29:29 +0100

    Add films list

--- src/app/pages/films/films.component.ts
+++ src/app/pages/films/films.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FilmService} from "../../services/film/film.service";
+import {Film} from "../../services/film/film";
 
 @Component({
   selector: 'app-films',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./films.component.scss']
 })
 export class FilmsComponent implements OnInit {
+  public films: Film[];
 
-  constructor() { }
+  constructor(private filmService: FilmService) { }
 
   ngOnInit(): void {
+    this.filmService.getFilms().subscribe(data => {
+      this.films = data;
+    });
   }
 
 }
