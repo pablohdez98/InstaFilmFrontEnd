a2d2af12764f50b9bf2494afbc7480f751977021
Pablo Hernández <pablohc224@gmail.com>
2020-05-25 17:11:49 +0100

    Add route for series-details

--- src/app/pages/series/series-routing.module.ts
+++ src/app/pages/series/series-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { SeriesComponent } from './series.component';
+import {SeriesDetailsComponent} from './series-details/series-details.component';
 
-const routes: Routes = [{ path: '', component: SeriesComponent }];
+const routes: Routes = [
+  { path: '', component: SeriesComponent },
+  { path: ':id', component: SeriesDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
