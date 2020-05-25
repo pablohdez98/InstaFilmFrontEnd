7a2a775d9f719d65fbf57efe63b5bcf921e76c29
Pablo Hernández <pablohc224@gmail.com>
2020-05-25 17:11:17 +0100

    Add series-details compònent

---
+++ src/app/pages/series/series-details/series-details.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {SeriesService} from '../../../services/series/series.service';
+import {ActivatedRoute} from '@angular/router';
+import {Series} from '../../../services/series/series';
+
+@Component({
+  selector: 'app-series-details',
+  templateUrl: './series-details.component.html',
+  styleUrls: ['./series-details.component.scss']
+})
+export class SeriesDetailsComponent implements OnInit {
+  public series: Series;
+
+  constructor(private seriesService: SeriesService,
+              private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.seriesService.getSeries(params.id).subscribe(series => {
+        console.log(series);
+        this.series = series;
+      });
+    });
+  }
+}
