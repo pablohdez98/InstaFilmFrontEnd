e32246acb5f2d4b19d02b6314b2fec5e8342cf39
Javier Sintes <javier@secret-source.eu>
2020-05-28 10:23:56 +0100

    Import ReactiveFormsModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {SafePipe} from '../pipes/safe.pipe';
-import {CommentsListComponent} from '../comments-list/comments-list.component';
+import {CommentsListComponent} from './comments-list/comments-list.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 
 
 @NgModule({
   declarations: [SafePipe, CommentsListComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
   exports: [SafePipe, CommentsListComponent]
 })
